669b40800fb3b0e2b1f87c5cdfc8c74f0e6d7bea
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-06-22 02:43:31 +0900

    Python Tutorial 07 Variables
    We learn  how to make variable what is static programming and dynamic programming

---
+++ 04_variable/main.py
@@ -0,0 +1,13 @@
+
+
+#Math 
+# x = (12 + y)  
+
+age = 26 # this is age variable with int 
+name = "john" # this is name variable with string
+salary = 15.00 # this is double 
+
+print(age)
+print(name)
+print(salary)
+
