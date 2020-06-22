f43b6903d533fd9644bbe26bc578cd02c8cadc60
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-06-22 12:05:47 +0900

    Python Tutorial 11  Numeric Type
    Python3 number data type we learn about the best github account to get hackings tools

---
+++ 08_numeric/main.py
@@ -0,0 +1,21 @@
+
+
+
+
+#Python number
+# int
+# float
+# complex
+
+x = 1
+y = 13.23
+z = 1j
+
+print(x)
+print(y)
+print(z)
+print("-----------------------------------")
+print(type(x))
+print(type(y))
+print(type(z))
+print("-----------------------------------")
