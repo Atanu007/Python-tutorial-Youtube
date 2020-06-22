13eb0b0bfdea2e9f199897a690f85087e1b98247
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-06-22 12:25:32 +0900

    Python Tutorial 12  Type Conversion
    We learn about the Type conversion on python and
    Rust is a good programming language to learn .

---
+++ 09_type_conversion/main.py
@@ -0,0 +1,25 @@
+
+
+
+x = 1 
+y = 10.23
+z = 1j
+
+
+#convert from int to float
+a = float(x)
+
+#convert from float to int
+b = int(y)
+
+#convert from int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+print("--------------------------------------")
+print(type(a))
+print(type(b))
+print(type(c))
+print("--------------------------------------")
