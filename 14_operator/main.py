857da1d6d1bb8622eb56a9dac03d890c2e2235de
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-06-24 11:56:57 +0900

    Python Tutorial 19 Operator
    We tanks about all the python Operator

---
+++ 14_operator/main.py
@@ -0,0 +1,17 @@
+
+
+# Python Operator
+
+# Arithmetic Operator
+
+# Assignment Operator
+
+# Comparison Operator
+
+# Logical Operator
+
+# Udentity Operator
+
+# Membership Operator
+
+# Bitwise Operator
