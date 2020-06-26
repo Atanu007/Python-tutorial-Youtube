551b0568b94f5a03f76c078c28655cf90924fbbc
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-06-26 10:22:59 +0900

    Python Tutorial 20 Arithmetic operators
    We Learn about the Arithnetic operator

---
+++ 15_arithmetic_operator/main.py
@@ -0,0 +1,13 @@
+
+print("This is arithmetic_operator")
+print("---------------------------")
+print(1 + 1 )       #Addition
+print(12 - 1)       #substraction
+print(12 * 1)       #Multiplication
+print(12 /2)        #Division
+print("---------------------------")
+
+print(5 % 2)        #Modulus
+print(12 ** 2)      #Exponentation
+print(15 // 2)      #Floor Division
+print("---------------------------")
